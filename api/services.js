905abb73d54bd3e115edec55a9dd1d39ceedeeee
taxcentrezm <taxcentrezm@gmail.com>
2025-10-28 19:32:53 +0200

    services

--- api/services.js
+++ api/services.js
@@ -1,9 +1,12 @@
-import { client } from "../../db.js";
+import { client } from "../db.js";
 
 export default async function handler(req, res) {
   try {
     const result = await client.execute("SELECT * FROM services WHERE clinic_id = 1;");
-    res.status(200).json({ message: "Services fetched successfully", data: result.rows });
+    res.status(200).json({
+      message: "Services fetched successfully",
+      data: result.rows
+    });
   } catch (err) {
     console.error("❌ API Error (services):", err.message);
     res.status(500).json({ error: err.message });
