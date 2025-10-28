3196f357093f3318831aaf6a4d13fa71e05d6b5a
taxcentrezm <taxcentrezm@gmail.com>
2025-10-28 17:06:44 +0200

    Create db.js

---
+++ db.js
@@ -0,0 +1,15 @@
+import { createClient } from "@libsql/client/web";
+
+export const client = createClient({
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+(async () => {
+  try {
+    await client.execute("SELECT 1;");
+    console.log("✅ Turso DB connected");
+  } catch (err) {
+    console.error("❌ Turso DB connection failed:", err.message);
+  }
+})();
