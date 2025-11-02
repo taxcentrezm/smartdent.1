4045c8b4c5825d4daaa3933903436110260a7e7c
taxcentrezm <taxcentrezm@gmail.com>
2025-11-02 16:57:49 +0200

    Update stock.js

--- api/stock.js
+++ api/stock.js
@@ -51,10 +51,7 @@ export default async function handler(req, res) {
         return res.status(400).json({ error: "Invalid type for GET" });
       }
 
-        if (type === "catalog") {
-  const itemsRes = await client.execute("SELECT * FROM supplier_items;");
-  return res.status(200).json({ data: itemsRes.rows });
-}
+  
 
 
       // ================= POST =================
